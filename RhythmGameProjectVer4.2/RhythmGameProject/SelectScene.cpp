2b8ab785cbfeed229131c7a2428bcfaa48273dc0
AntarticPenguin <kes99709@gmail.com>
2018-04-12 14:09:33 +0900

    180411 스크롤뷰 구현
    스크롤뷰 구현

--- RhythmGameProjectVer4.2/RhythmGameProject/SelectScene.cpp
+++ RhythmGameProjectVer4.2/RhythmGameProject/SelectScene.cpp
@@ -14,7 +14,7 @@ SelectScene::SelectScene()
 	_selectPosX = 515;
 	_selectPosY = 100;
 
-	_viewIndex = 0;
+	_selectIndex = 0;
 	_startViewIndex = 0;
 	_endViewIndex = 6;
 }
@@ -107,6 +107,76 @@ void SelectScene::InitViewList()
 		infoBox->SetPosition(300, i * 100 + 75);
 		_viewList.push_back(infoBox);
 	}
+	
+	if (6 < _musicList.size())
+	{
+		_endViewIndex = 6;
+		_endPosY = 700;
+	}
+	else
+	{
+		_endViewIndex = _musicList.size() - 1;
+		_endPosY = _musicList.size() * 100;
+	}
+		
+}
+
+void SelectScene::SelectUp()
+{
+	//SelectBox 위치
+	_selectPosY -= 100;
+	if (_selectPosY <= 100)
+		_selectPosY = 100;
+
+	_selectIndex--;
+	if (_selectIndex < _startViewIndex)
+	{
+		_startViewIndex--;
+		if(_startViewIndex < 0)
+		{
+			_startViewIndex = 0;
+			_selectIndex = _startViewIndex;
+			return;
+		}
+		_endViewIndex = _startViewIndex + 6;
+
+		//위치 조정
+		int index = 0;
+		for (int i = _startViewIndex; i < _endViewIndex + 1; i++)
+		{
+			_viewList[i]->SetPosition(300, index * 100 + 75);
+			index++;
+		}
+	}
+}
+
+void SelectScene::SelectDown()
+{
+	//SelectBox 위치
+	_selectPosY += 100;
+	if (_endPosY <= _selectPosY)
+		_selectPosY = _endPosY;
+	
+	_selectIndex++;
+	if (_endViewIndex < _selectIndex)
+	{
+		_endViewIndex++;
+		if (_viewList.size() <= _endViewIndex)
+		{
+			_endViewIndex = _viewList.size() - 1;
+			_selectIndex = _endViewIndex;
+			return;
+		}
+		_startViewIndex = _endViewIndex - 6;
+
+		//위치 조정
+		int index = 0;
+		for (int i = _startViewIndex; i < _viewList.size(); i++)
+		{
+			_viewList[i]->SetPosition(300, index * 100 + 75);
+			index++;
+		}
+	}
 }
 
 void SelectScene::KeyDown(int keyCode)
@@ -120,35 +190,10 @@ void SelectScene::KeyDown(int keyCode)
 		SceneManager::GetInstance()->ChangeScene(eScene::SCENE_TITLE);
 		break;
 	case SDLK_UP:
-		_viewIndex--;
-		if (_viewIndex < _startViewIndex)
-			_viewIndex = _startViewIndex;
-		printf("index: %d\n", _viewIndex);
-
-		//SelectBox 위치
-		_selectPosY -= 100;
-		if (_selectPosY <= 100)
-			_selectPosY = 100;
+		SelectUp();
 		break;
 	case SDLK_DOWN:
-		_viewIndex++;
-
-		//밑에 더 리스트가 있는지 체크
-		//있으면 startIndex, endIndex 증가
-		if (NULL != _viewList[_viewIndex])
-		{
-			_startViewIndex++;
-			_endViewIndex++;
-		}
-
-		if (_endViewIndex <= _viewIndex)
-			_viewIndex = _endViewIndex;
-		printf("index: %d\n", _viewIndex);
-
-		//SelectBox 위치
-		_selectPosY += 100;
-		if (700 <= _selectPosY)
-			_selectPosY = 700;
+		SelectDown();
 		break;
 	}
 }
